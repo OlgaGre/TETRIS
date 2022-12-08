d236445bbc1bb716229b12b4cf01be315608c247
OlgaGre <grechanyuko@gmail.com>
2022-12-08 03:20:16 +0300

    feat: color full and empty squares

--- my-app/src/components/cup/Cup.jsx
+++ my-app/src/components/cup/Cup.jsx
@@ -4,12 +4,12 @@ const Cup = (props) => {
   let buildMarkupCup = props.markupCup.map((el) => (
     <div className={styles.line}>
       {el.map((el) => (
-        <div>{el}</div>
+        <div className={`${el == 1 ? styles.fullSquare: styles.square}`}></div>
       ))}
     </div>
   ));
   console.log(props.markupCup);
   return (
-    <div>{buildMarkupCup}</div>)
+    <div className={styles.cup}>{buildMarkupCup}</div>)
 };
 export default Cup;
